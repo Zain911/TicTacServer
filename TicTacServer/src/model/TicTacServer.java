c8e5cebe16a9b5b07a503373f2599119074ed8a8
mohamed Galal <mohamedgalalelsheikh11@gmail.com>
2021-12-30 12:09:30 -0800

    server GUI

--- TicTacServer/src/model/TicTacServer.java
+++ TicTacServer/src/model/TicTacServer.java
@@ -1,9 +1,11 @@
+package model;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,11 +21,12 @@ public class TicTacServer extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setTitle("Tic Tac Toe Server");
         stage.show();
     }
 
